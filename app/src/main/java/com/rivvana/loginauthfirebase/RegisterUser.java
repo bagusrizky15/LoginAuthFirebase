109e45bd816ad04120c36964e17475b03ac9e01e
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2022-03-13 10:56:11 +0700

    firebaseauth

--- app/src/main/java/com/rivvana/loginauthfirebase/RegisterUser.java
+++ app/src/main/java/com/rivvana/loginauthfirebase/RegisterUser.java
@@ -1,5 +1,6 @@
 package com.rivvana.loginauthfirebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -9,8 +10,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterUser extends AppCompatActivity implements View.OnClickListener{
 
@@ -58,19 +64,19 @@ public class RegisterUser extends AppCompatActivity implements View.OnClickListe
         if (fullName.isEmpty()){
             editTextFullName.setError("Fullname is required!");
             editTextFullName.requestFocus();
-            return;;
+            return;
         }
 
         if (email.isEmpty()){
             editTextEmail.setError("Email is required!");
             editTextEmail.requestFocus();
-            return;;
+            return;
         }
 
-        if (Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
             editTextEmail.setError("Please input valid email");
             editTextEmail.requestFocus();
-            return;;
+            return;
         }
 
         if (password.isEmpty()){
@@ -84,6 +90,37 @@ public class RegisterUser extends AppCompatActivity implements View.OnClickListe
             editTextPassword.requestFocus();
             return;
         }
+
+        progressBar.setVisibility(View.VISIBLE);
+        mAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            User user = new User(fullName, email);
+
+                            FirebaseDatabase.getInstance().getReference("Users")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(RegisterUser.this, "User has been Registered Successfully!", Toast.LENGTH_LONG).show();
+                                        progressBar.setVisibility(View.VISIBLE);
+
+                                    } else {
+                                        Toast.makeText(RegisterUser.this, "Failed to Register, Try Again!", Toast.LENGTH_LONG).show();
+                                        progressBar.setVisibility(View.GONE);
+                                    }
+                                }
+                            });
+
+                        } else {
+                            Toast.makeText(RegisterUser.this, "Failed to Register, Try Again!", Toast.LENGTH_LONG).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
 
     }
 }
