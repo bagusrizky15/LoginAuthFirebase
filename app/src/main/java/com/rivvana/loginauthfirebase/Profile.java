29a0f4137f71f2a0b04213fe54830001376ce168
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2022-03-17 17:19:28 +0700

    added logout

--- app/src/main/java/com/rivvana/loginauthfirebase/Profile.java
+++ app/src/main/java/com/rivvana/loginauthfirebase/Profile.java
@@ -2,13 +2,30 @@ package com.rivvana.loginauthfirebase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Profile extends AppCompatActivity {
+
+    private Button logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        logout = (Button) findViewById(R.id.logoutBtn);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(Profile.this, MainActivity.class));
+            }
+        });
     }
 }
